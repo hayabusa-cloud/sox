a00b2007fe14a8970bd7f46c6c73a08bd548feb1
hayabusa-cloud <git@hybscloud.com>
2022-12-04 18:06:36 +0900

    feat: (WIP)implement spin waiter

---
+++ spin_waiter.go
@@ -0,0 +1,84 @@
+package sox
+
+import (
+	"math"
+	"runtime"
+	"time"
+)
+
+const (
+	SpinWaitLevelClient = iota
+	SpinWaitLevelBlockingIO
+	SpinWaitLevelConsume
+	SpinWaitLevelProduce
+	SpinWaitLevelAtomic
+	spinWaitLevelNoSleep
+)
+
+type SpinWaiter struct {
+	i     uint32
+	level int8
+	d     time.Duration
+	limit uint32
+	total int32
+}
+
+func NewSpinWaiter() *SpinWaiter {
+	return &SpinWaiter{
+		i:     0,
+		level: SpinWaitLevelBlockingIO,
+		d:     jiffies,
+		limit: 0,
+		total: 0,
+	}
+}
+
+func (sw *SpinWaiter) SetLevel(level int) *SpinWaiter {
+	if level < SpinWaitLevelClient {
+		sw.level = SpinWaitLevelClient
+		return sw
+	}
+	if level > spinWaitLevelNoSleep {
+		level = spinWaitLevelNoSleep
+	}
+	sw.level = int8(level)
+
+	return sw
+}
+
+func (sw *SpinWaiter) SetLimit(limit int) *SpinWaiter {
+	if limit > math.MaxUint32-1 {
+		limit = math.MaxUint32 - 1
+	}
+	sw.limit = uint32(limit)
+
+	return sw
+}
+
+func (sw *SpinWaiter) SetDuration(d time.Duration) *SpinWaiter {
+	if d < jiffies {
+		d = jiffies
+	}
+	sw.d = d
+
+	return sw
+}
+
+func (sw *SpinWaiter) Closed() bool {
+	return sw.limit > 0 && sw.i >= sw.limit
+}
+
+func (sw *SpinWaiter) Once() {
+	d := int32(0)
+	for i := sw.i; i > 0; i &= i - 1 {
+		d++
+	}
+	d = d >> sw.level
+	if d > 0 {
+		sw.total += d
+		time.Sleep(time.Duration(d) * sw.d)
+	} else {
+		runtime.Gosched()
+	}
+	sw.i++
+}
---
+++ spin_waiter_test.go
@@ -0,0 +1,108 @@
+package sox
+
+import (
+	"math"
+	"runtime"
+	"sync/atomic"
+	"testing"
+	"time"
+)
+
+func TestSpinWaiter(t *testing.T) {
+	fn := func(x *atomic.Int32) {
+		for {
+			val := x.Load()
+			if val < 0 {
+				break
+			}
+			if val > math.MaxInt32/2 {
+				val = math.MaxInt32 / 2
+			}
+			if !x.CompareAndSwap(val, val+1) {
+				runtime.Gosched()
+				continue
+			}
+			if val > math.MaxInt16 {
+				time.Sleep(jiffies)
+			}
+		}
+	}
+	t.Run("common usage", func(t *testing.T) {
+		x := atomic.Int32{}
+		go fn(&x)
+		for sw := NewSpinWaiter(); !sw.Closed(); sw.Once() {
+			val := x.Load()
+			//  some actions
+			runtime.Gosched()
+
+			if !x.CompareAndSwap(val, -1) {
+				continue
+			}
+			break
+		}
+		res := x.Load()
+		if res != -1 {
+			t.Errorf("wronf value of x expected %d but got %d", -1, res)
+			return
+		}
+	})
+
+	t.Run("level 0", func(t *testing.T) {
+		sw := NewSpinWaiter().SetLevel(SpinWaitLevelClient)
+		for i := 0; i < 1<<8; i++ {
+			sw.Once()
+		}
+		if sw.total != 1<<10 {
+			t.Errorf("expected total wait %d but got %d", 1024, sw.total)
+		}
+	})
+
+	t.Run("level 1", func(t *testing.T) {
+		sw := NewSpinWaiter().SetLevel(SpinWaitLevelBlockingIO)
+		for i := 0; i < 1<<9; i++ {
+			sw.Once()
+		}
+		if sw.total != 1<<10 {
+			t.Errorf("expected total wait %d but got %d", 1<<10, sw.total)
+		}
+	})
+
+	t.Run("level 2", func(t *testing.T) {
+		sw := NewSpinWaiter().SetLevel(SpinWaitLevelConsume)
+		for i := 0; i < 1<<10; i++ {
+			sw.Once()
+		}
+		if sw.total <= 1<<9 || sw.total >= 1<<10 {
+			t.Errorf("expected total wait between %d and %d but got %d", 1<<9, 1<<10, sw.total)
+		}
+	})
+
+	t.Run("level 3", func(t *testing.T) {
+		sw := NewSpinWaiter().SetLevel(SpinWaitLevelProduce)
+		for i := 0; i < 1<<12; i++ {
+			sw.Once()
+		}
+		if sw.total <= 1<<9 || sw.total >= 1<<10 {
+			t.Errorf("expected total wait between %d and %d but got %d", 1<<9, 1<<10, sw.total)
+		}
+	})
+
+	t.Run("level 4", func(t *testing.T) {
+		sw := NewSpinWaiter().SetLevel(SpinWaitLevelAtomic)
+		for i := 0; i < 1<<16; i++ {
+			sw.Once()
+		}
+		if sw.total != 1 {
+			t.Errorf("expected total wait %d but got %d", 1, sw.total)
+		}
+	})
+
+	t.Run("timeout", func(t *testing.T) {
+		sw := NewSpinWaiter().SetLimit(128)
+		for ; !sw.Closed(); sw.Once() {
+		}
+		if sw.i != 128 {
+			t.Errorf("expected spin waiter wait %d times but executed %d times", 64, sw.i)
+		}
+	})
+}
