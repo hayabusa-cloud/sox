d537aac9dda8012e8c17ca928c1d7c152cdca616
hayabusa-cloud <git@hybscloud.com>
2023-01-29 22:11:39 +0900

    feat: add pollfd interfaces

--- poller.go
+++ poller.go
@@ -34,17 +34,98 @@ type poller interface {
 }
 
 type pollFd interface {
+	// Fd returns the file descriptor
 	Fd() int
 }
 
+// PollReader is the interface that groups Fd and the basic Read method
 type PollReader interface {
 	pollFd
 	io.Reader
 }
 
+// PollUintReader is the interface that groups ReadUint()
+// and the methods in interface PollReader
+type PollUintReader interface {
+	PollReader
+	ReadUint() (val uint, err error)
+}
+
+// PollWriter is the interface that groups Fd and the basic Write method
 type PollWriter interface {
 	pollFd
 	io.Writer
+}
+
+// PollUintWriter is the interface that groups WriteUint()
+// and the methods in interface PollWriter
+type PollUintWriter interface {
+	PollWriter
+	WriteUint(val uint) error
+}
+
+// PollReadWriter is the interface that groups the methods
+// in interface PollReader and PollWriter
+type PollReadWriter interface {
+	PollReader
+	PollWriter
+}
+
+// PollUintReadWriter is the interface that groups the methods
+// in interface PollUintReader and PollUintWriter
+type PollUintReadWriter interface {
+	PollUintReader
+	PollUintWriter
+}
+
+// PollCloser is the interface that groups Fd and the basic Close method
+type PollCloser interface {
+	pollFd
+	io.Closer
+}
+
+// PollReadCloser is the interface that groups the methods
+// in interface PollReader and PollCloser
+type PollReadCloser interface {
+	PollReader
+	io.Closer
+}
+
+// PollWriteCloser is the interface that groups the methods
+// in interface PollWriter and PollCloser
+type PollWriteCloser interface {
+	PollWriter
+	io.Closer
+}
+
+// PollReadWriteCloser is the interface that groups the methods
+// in interface PollReader, PollWriter and PollCloser
+type PollReadWriteCloser interface {
+	PollReader
+	PollWriter
+	io.Closer
+}
+
+// PollUintReadCloser is the interface that groups the methods
+// in interface PollUintReader and PollUintCloser
+type PollUintReadCloser interface {
+	PollUintReader
+	io.Closer
+}
+
+// PollUintWriteCloser is the interface that groups the methods
+// in interface PollUintWriter and PollCloser
+type PollUintWriteCloser interface {
+	PollUintWriter
+	io.Closer
+}
+
+// PollUintReadWriteCloser is the interface that groups the methods
+// in interface PollUintReader, PollUintWriter and PollCloser
+type PollUintReadWriteCloser interface {
+	PollUintReader
+	PollUintWriter
+	io.Closer
 }
 
 type file interface {
