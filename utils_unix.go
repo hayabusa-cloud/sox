8595abfcaf566a1e7e703fd00bc9514678211d65
hayabusa-cloud <git@hybscloud.com>
2024-07-06 21:05:20 +0900

    feat: convert byte slices to unix iovecs utils

--- utils_unix.go
+++ utils_unix.go
@@ -11,6 +11,8 @@ import (
 	"unsafe"
 )
 
+type ioVec = unix.Iovec
+
 func errFromUnixErrno(err error) error {
 	if err == nil {
 		return nil
@@ -48,9 +50,86 @@ func errFromUnixErrno(err error) error {
 func ioVecFromBytesSlice(iov [][]byte) (addr uintptr, n int) {
 	vec := make([]unix.Iovec, len(iov))
 	for i := range len(iov) {
-		vec[i] = unix.Iovec{Base: &iov[i][0], Len: uint64(len(iov[i]))}
+		vec[i] = unix.Iovec{Base: unsafe.SliceData(iov[i]), Len: uint64(len(iov[i]))}
 	}
-	addr, n = uintptr(unsafe.Pointer(&iov)), len(iov)
+	addr, n = uintptr(unsafe.Pointer(unsafe.SliceData(vec))), len(vec)
 
 	return
+}
+
+func ioVecAddrLen(vec []ioVec) (addr uintptr, n int) {
+	addr, n = uintptr(unsafe.Pointer(unsafe.SliceData(vec))), len(vec)
+	return
+}
+
+func ioVecFromPicoBuffers(buffers []PicoBuffer) []ioVec {
+	vec := make([]ioVec, len(buffers))
+	for i := range len(buffers) {
+		vec[i] = ioVec{Base: (*byte)(unsafe.Pointer(&buffers[i])), Len: BufferSizePico}
+	}
+
+	return vec
+}
+
+func ioVecFromNanoBuffers(buffers []NanoBuffer) []ioVec {
+	vec := make([]ioVec, len(buffers))
+	for i := range len(buffers) {
+		vec[i] = ioVec{Base: (*byte)(unsafe.Pointer(&buffers[i])), Len: BufferSizeNano}
+	}
+
+	return vec
+}
+
+func ioVecFromMicroBuffers(buffers []MicroBuffer) []ioVec {
+	vec := make([]ioVec, len(buffers))
+	for i := range buffers {
+		vec[i] = ioVec{Base: (*byte)(unsafe.Pointer(&buffers[i])), Len: BufferSizeMicro}
+	}
+
+	return vec
+}
+
+func ioVecFromSmallBuffers(buffers []SmallBuffer) []ioVec {
+	vec := make([]ioVec, len(buffers))
+	for i := range buffers {
+		vec[i] = ioVec{Base: (*byte)(unsafe.Pointer(&buffers[i])), Len: BufferSizeSmall}
+	}
+
+	return vec
+}
+
+func ioVecFromMediumBuffers(buffers []MediumBuffer) []ioVec {
+	vec := make([]ioVec, len(buffers))
+	for i := range buffers {
+		vec[i] = ioVec{Base: (*byte)(unsafe.Pointer(&buffers[i])), Len: BufferSizeMedium}
+	}
+
+	return vec
+}
+
+func ioVecFromLargeBuffers(buffers []LargeBuffer) []ioVec {
+	vec := make([]ioVec, len(buffers))
+	for i := range buffers {
+		vec[i] = ioVec{Base: (*byte)(unsafe.Pointer(&buffers[i])), Len: BufferSizeLarge}
+	}
+
+	return vec
+}
+
+func ioVecFromHugeBuffers(buffers []HugeBuffer) []ioVec {
+	vec := make([]ioVec, len(buffers))
+	for i := range buffers {
+		vec[i] = ioVec{Base: (*byte)(unsafe.Pointer(&buffers[i])), Len: BufferSizeHuge}
+	}
+
+	return vec
+}
+
+func ioVecFromGiantBuffers(buffers []GiantBuffer) []ioVec {
+	vec := make([]ioVec, len(buffers))
+	for i := range buffers {
+		vec[i] = ioVec{Base: (*byte)(unsafe.Pointer(&buffers[i])), Len: BufferSizeGiant}
+	}
+
+	return vec
 }
