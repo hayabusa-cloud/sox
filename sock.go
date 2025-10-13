414ddfca62cb2cc13226eca40e6ee8dc11d93fb5
hayabusa-cloud <git@hybscloud.com>
2025-10-13 21:42:01 +0900

    feat: add Dial and SocketPair methods

--- sock.go
+++ sock.go
@@ -82,3 +82,94 @@ func ListenSCTP(network string, laddr *SCTPAddr) (listener Listener, err error) 
 	}
 	return nil, &OpError{Op: "listen", Net: network, Source: nil, Addr: laddr, Err: UnknownNetworkError(network)}
 }
+
+func Dial(network string, address string) (conn Conn, err error) {
+	switch network {
+	case "tcp":
+		a, err := ResolveTCPAddr(network, address)
+		if err != nil {
+			return nil, err
+		}
+		if ipFamily(a.IP) == NetworkIPv4 {
+			return DialTCP4(nil, a)
+		}
+		return DialTCP6(nil, a)
+	case "tcp4":
+		a, err := ResolveTCPAddr(network, address)
+		if err != nil {
+			return nil, err
+		}
+		return DialTCP4(nil, a)
+	case "tcp6":
+		a, err := ResolveTCPAddr(network, address)
+		if err != nil {
+			return nil, err
+		}
+		return DialTCP6(nil, a)
+	case "udp":
+		a, err := ResolveUDPAddr(network, address)
+		if err != nil {
+			return nil, err
+		}
+		if ipFamily(a.IP) == NetworkIPv4 {
+			return DialUDP4(nil, a)
+		}
+		return DialUDP6(nil, a)
+	case "udp4":
+		a, err := ResolveUDPAddr(network, address)
+		if err != nil {
+			return nil, err
+		}
+		return DialUDP4(nil, a)
+	case "udp6":
+		a, err := ResolveUDPAddr(network, address)
+		if err != nil {
+			return nil, err
+		}
+		return DialUDP6(nil, a)
+	case "sctp":
+		a, err := ResolveSCTPAddr(network, address)
+		if err != nil {
+			return nil, err
+		}
+		if ipFamily(a.IP) == NetworkIPv4 {
+			return DialSCTP4(nil, a)
+		}
+		return DialSCTP6(nil, a)
+	case "sctp4":
+		a, err := ResolveSCTPAddr(network, address)
+		if err != nil {
+			return nil, err
+		}
+		return DialSCTP4(nil, a)
+	case "sctp6":
+		a, err := ResolveSCTPAddr(network, address)
+		if err != nil {
+			return nil, err
+		}
+		return DialSCTP6(nil, a)
+	case "unix", "unixpacket":
+		addr, err := ResolveUnixAddr(network, address)
+		if err != nil {
+			return nil, err
+		}
+		return DialUnix(nil, addr)
+	}
+
+	return nil, &OpError{Op: "dial", Net: network, Source: nil, Addr: nil, Err: UnknownNetworkError(network)}
+}
+
+func SocketPair(network string) (so [2]Socket, err error) {
+	switch network {
+	case "unix", "unixpacket":
+		uSockets, err := newUnixSocketPair()
+		if err != nil {
+			return [2]Socket{}, err
+		}
+		so[0] = uSockets[0]
+		so[1] = uSockets[1]
+		return so, nil
+	}
+
+	return [2]Socket{}, &OpError{Op: "socketpair", Net: network, Source: nil, Addr: nil, Err: UnknownNetworkError(network)}
+}
